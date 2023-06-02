25beb0269f408d39605fdc826f4a0cbf338c5261
Gabor Juhasz <gabor.juhasz@accretivetg.com>
2023-06-02 12:55:38 +0200

    move passwords to env

--- app/admin/login/page.tsx
+++ app/admin/login/page.tsx
@@ -6,7 +6,7 @@ const AdminLogin = async () => {
     "use server";
     const password = data.get("password");
 
-    if (password === "supersecure") {
+    if (password === process.env.DD_ADMIN_PASSWORD) {
       cookies().set("bvj-secure", "1", {
         httpOnly: true,
         secure: true,
@@ -20,7 +20,7 @@ const AdminLogin = async () => {
   return (
     <div className="flex h-screen items-center justify-center bg-gray-800">
       <form className="flex flex-col gap-4" action={onSubmit}>
-        <input className="rounded p-1 text-black" type="password" name="password" />
+        <input className="rounded p-1 text-black" autoFocus type="password" name="password" />
         <button className="rounded bg-blue-500 p-1">GO</button>
       </form>
     </div>
